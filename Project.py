9750be10086f8a1f1757e62e0f943eebcc944b35
pragyagautam02 <pragyagautam020801@gmail.com>
2023-03-07 12:35:30 +0530

    New changes

--- Project.py
+++ Project.py
@@ -14,10 +14,81 @@ from xlwt import Workbook
 
 
 ## created open AI key
-openai.api_key = "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"
+openai.api_key = "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"
 
-## function defined to extract the data
-def extractData(file):
+## ============================================================================================================================================
+## function defined to extract the data in xls format
+def xls_extract(data):
+    # Workbook is created
+    wb = Workbook()
+
+    # creating sheet for measures
+    Measures = wb.add_sheet('Measures')
+    Measures.write(0, 0, 'Measure Name')
+    Measures.write(0, 1, 'Measure Expression')
+    Measures.write(0, 2, 'Measure Description')
+
+    ## ----------------------------------------------------- MEASURES ----------------------------------------------------------------------
+    cnt = 0
+    for t in data['model']['tables']:
+        if 'measures' in t:
+            list_measures = t['measures']
+            for i in list_measures:
+                prompt = "Explain the following calculation in a few sentences in simple business terms without using DAX function names: " + i['expression']
+                completion = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=64)
+                cnt += 1
+                Measures.write(cnt, 0, i['name'])
+                Measures.write(cnt, 1, i['expression'])
+                Measures.write(cnt, 2, completion.choices[0]['text'].strip())
+
+    ## ----------------------------------------------------- SOURCE INFORMATION ----------------------------------------------------------------------
+    i = 0
+    # creating sheet for source information
+    source = wb.add_sheet('Source Information')
+    source.write(0, 0, 'Table No.')
+    source.write(0, 1, 'Table Name')
+    source.write(0, 2, 'Source')
+    for t in data['model']['tables']:
+        if 'partitions' in t:
+            list_partitions = t['partitions']
+            List_source = (list_partitions[0]['source']['expression'])
+            if List_source[0] == 'let':
+                name = list_partitions[0]['name'].split('-')[0]
+                i += 1
+                p = List_source[1]
+                source.write(i, 0, i)
+                source.write(i, 1, name)
+                source.write(i, 2, p)
+
+    ## ----------------------------------------------------- RELATIOPNSHIPS ----------------------------------------------------------------------
+    relation = wb.add_sheet('Relationships')
+    relation.write(0, 0, 'From Table')
+    relation.write(0, 1, 'From Column')
+    relation.write(0, 2, 'To Table')
+    relation.write(0, 3, 'To Column')
+    cnt1 = 0
+
+    for t in data['model']['relationships']:
+        if "joinOnDateBehavior" not in t:
+            cnt1 += 1
+            relation.write(cnt1, 0, t['fromTable'])
+            relation.write(cnt1, 1, t['fromColumn'])
+            relation.write(cnt1, 2, t['toTable'])
+            relation.write(cnt1, 3, t['toColumn'])
+        if "crossFilteringBehavior" in t:
+            relation.write(0, 4, 'Direction')
+            relation.write(cnt1, 4, t['crossFilteringBehavior'])
+
+        if "toCardinality" in t:
+            relation.write(0, 5, 'Cardinality')
+            relation.write(cnt1, 5, t['toCardinality'])
+
+    ## saving the excel workbook file
+    wb.save('Details.xls')
+
+## ============================================================================================================================================
+## function defined to extract the data in json format
+def json_extract(file):
     ## getting the directory name
     dir_name = os.path.dirname(file)
 
@@ -56,9 +127,9 @@ def extractData(file):
 
     out_file.close()
 
+    xls_extract(data)
 
-
-
+## --------------------------------------------------------------- MAIN -------------------------------------------------------------------------
 ## printing the menu
 print("Do you want to select file or folder\n1.File\n2.Folder")
 
@@ -69,7 +140,7 @@ if(op==1):
     # opening file dialog to select file
     file = filedialog.askopenfilename(title="Select file")
     print(file)
-    extractData(file)
+    json_extract(file)
 else:
     ## opening file dialog to select folder
     folder = askdirectory(title="Select folder")
@@ -90,92 +161,6 @@ else:
             file_path = f"{folder}/{f}"
             print(file_path)
             # read_files(file_path)
-            extractData(file_path)
+            json_extract(file_path)
 
-#
-# # Workbook is created
-# wb = Workbook()
-# # creating sheet for measures
-# Measures = wb.add_sheet('Measures')
-# Measures.write(0, 0, 'Measure Name')
-# Measures.write(0, 1, 'Measure Expression')
-# Measures.write(0, 2, 'Measure Description')
-#
-# # print("\n")
-# # print("MEASURES")
-# cnt = 0
-# for t in data['model']['tables']:
-#     if 'measures' in t:
-#         list_measures = t['measures']
-#         for i in list_measures:
-#             prompt="Explain the following calculation in a few sentences in simple business terms without using DAX function names: "+i['expression']
-#             completion = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=64)
-#             # print("Measure Name : ", i['name'])
-#             # print("Measure Expression : ", i['expression'])
-#             # print("Measure Description: ", completion.choices[0]['text'].strip())
-#             cnt += 1
-#             # output.write(i['name'] + "," + i['expression'] + ", descript")
-#             # output.write('\n')
-#             Measures.write(cnt, 0, i['name'])
-#             Measures.write(cnt, 1, i['expression'])
-#             Measures.write(cnt, 2, completion.choices[0]['text'].strip())
-#
-#
-#
-# # print("\n")
-# # print("SOURCE OF INFORMATION")
-# i = 0
-# # creating sheet for source information
-# source = wb.add_sheet('Source Information')
-# source.write(0, 0, 'Table No.')
-# source.write(0, 1, 'Table Name')
-# source.write(0, 2, 'Source')
-# for t in data['model']['tables']:
-#     if 'partitions' in t:
-#         list_partitions= t['partitions']
-#         List_source = (list_partitions[0]['source']['expression'])
-#         if List_source[0] == 'let':
-#             name = list_partitions[0]['name'].split('-')[0]
-#             i += 1
-#             # print("Table " + str(i) + " Path :" + List_source[1])
-#             # p = List_source[1].split("(")[2].split(")")
-#             p = List_source[1]
-#             source.write(i, 0, i)
-#             source.write(i, 1, name)
-#             source.write(i, 2, p)
-#
-#
-# # print("\n")
-# # print("RELATIONSHIPS")
-# # creating sheet for source information
-# relation = wb.add_sheet('Relationships')
-# relation.write(0, 0, 'From Table')
-# relation.write(0, 1, 'From Column')
-# relation.write(0, 2, 'To Table')
-# relation.write(0, 3, 'To Column')
-# cnt1 = 0
-#
-# for t in data['model']['relationships']:
-#     # print("From Table : ", t['fromTable'])
-#     # print("From Column : ", t['fromColumn'])
-#     # print("To Table : ", t['toTable'])
-#     # print("To Column : ", t['toColumn'])
-#     # print("Direction : ", t['crossFilteringBehavior'])
-#     # print("Cardinality : ", t['toCardinality'])
-#     if "joinOnDateBehavior" not in t:
-#         cnt1 += 1
-#         relation.write(cnt1, 0, t['fromTable'])
-#         relation.write(cnt1, 1, t['fromColumn'])
-#         relation.write(cnt1, 2, t['toTable'])
-#         relation.write(cnt1, 3, t['toColumn'])
-#     if "crossFilteringBehavior" in t:
-#         relation.write(0, 4, 'Direction')
-#         relation.write(cnt1, 4, t['crossFilteringBehavior'])
-#
-#     if "toCardinality" in t:
-#         relation.write(0, 5, 'Cardinality')
-#         relation.write(cnt1, 5, t['toCardinality'])
-#
-#
-# wb.save('Details.xls')
-#
+
